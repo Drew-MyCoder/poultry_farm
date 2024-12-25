8c84d20dcd3c9a89d698611881d7e36a9821bb25
Drew-MyCoder <andrewsdwomoh9@gmail.com>
2024-12-25 19:49:00 +0000

    schema for expenditure

---
+++ api/expenditure/schema.py
@@ -0,0 +1,33 @@
+from pydantic import BaseModel
+from datetime import datetime
+
+
+class ExpenditureBase(BaseModel):
+    user_id: int
+
+
+class ExpenditureCreate(ExpenditureBase):
+    id: int
+    amount: int
+    reference: str
+
+
+class ExpenditureUpdate(ExpenditureBase):
+    amount: int
+    reference: str
+
+
+class Expenditure(BaseModel):
+    id: int
+    user_id: int
+    amount: int
+    reference: str
+
+
+class ExpenditureOutput(BaseModel):
+    id: int
+    user_id: int
+    amount: int
+    reference: str
+    created_at: datetime
+    updated_at: datetime
