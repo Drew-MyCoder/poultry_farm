b06a0da057dd65479b9093050259c055724fca33
Drew-MyCoder <andrewsdwomoh9@gmail.com>
2025-03-25 06:54:38 +0000

    databse config

--- database.py
+++ database.py
@@ -12,10 +12,16 @@ database_name = os.getenv("DATABASE_NAME")
 database_username = os.getenv("DATABASE_USERNAME")
 database_password = os.getenv("DATABASE_PASSWORD")
 database_host = os.getenv("DATABASE_HOST")
+print(f"DATABASE_NAME: {database_name}")
+print(f"DATABASE_USERNAME: {database_username}")
+# print(f"DATABASE_PASSWORD: {database_password}")  # Be careful with this in production!
+print(f"DATABASE_HOST: {database_host}")
 
 
+database_url: str = os.getenv("DATABASE_URL")  # Use Render's default DATABASE_URL
 
-database_url: str = f"postgresql://{database_username}:{database_password}@{database_host}/{database_name}"
+if not database_url:
+    database_url = f"postgresql://{database_username}:{database_password}@{database_host}/{database_name}"
 engine = create_engine(database_url)
 # Base.metadata.create_all(bind=engine)
 sessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
@@ -41,6 +47,9 @@ print(f"Database URL: {database_url}")
 
 try:
     with engine.connect() as connection:
+        inspector = inspect(engine)
+        tables = inspector.get_table_names()
+        print("Existing table: ", tables)
         print("Connected to the database")
         # result = connection.execute(text("SELECT * FROM users LIMIT 1;"))
         # for row in result:
