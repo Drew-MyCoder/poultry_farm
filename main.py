03dc56e53cc56d7ffb766fbfbef8bc97649f97e3
Drew-MyCoder <andrewsdwomoh9@gmail.com>
2025-03-25 10:51:50 +0000

    removed trailing slash

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ app = FastAPI(
 origins = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "https://stupendous-sherbet-004810.netlify.app/"
+    "https://stupendous-sherbet-004810.netlify.app"
 ]
 
 app.add_middleware(
